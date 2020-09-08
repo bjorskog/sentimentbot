e90ba3e7e7e42033b21462509f1001ea28944f70
bjorskog <bjorn.skogtro@gmail.com>
2020-09-08 16:20:25 +0200

    Added logging to task.

--- sentimentbot/tasks.py
+++ sentimentbot/tasks.py
@@ -7,12 +7,15 @@ batch, implemented using luigi.
 
 
 import luigi
+import logging
 import pathlib
 import datetime
 
 from sentimentbot.feeds import FinvizNewsFeed
 
 _OUTPATH = pathlib.Path().home().joinpath("data")
+
+log = logging.getLogger(__name__)
 
 
 class ReadFinviz(luigi.ExternalTask):
@@ -24,6 +27,7 @@ class ReadFinviz(luigi.ExternalTask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._feed = FinvizNewsFeed(self.ticker)
+        log.info("Reading feed {}.".format(self._feed.uri))
         date_string = self.date_time.strftime("%Y%m%d%H%M")
         self._out_file = _OUTPATH.joinpath(
             f"finviz_news_{self.ticker.lower()}_{date_string}.csv"
